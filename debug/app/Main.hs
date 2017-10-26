e6394a52c68340554a75bc860e47831a311b3b62
Jacob Errington <git@mail.jerrington.me>
2017-10-26 08:59:37 -0400

    add debug project to run the whole thing

---
+++ debug/app/Main.hs
@@ -0,0 +1,45 @@
+module Main where
+
+import Data.Proxy
+import qualified Data.ByteString as BS
+import Network.HTTP.ReverseProxy
+import Network.HTTP.Client ( Manager, defaultManagerSettings, newManager )
+import Network.Wai
+import Network.Wai.Handler.Warp ( run )
+import Network.Wai.Middleware.RequestLogger ( logStdoutDev )
+import Servant
+import Servant.Utils.StaticFiles
+import System.Environment ( getEnv )
+
+forwardRequest :: ProxyDest -> Request -> IO WaiProxyResponse
+forwardRequest dest req = do
+  print req
+  pure (WPRModifiedRequest req' dest) where
+    req' = req { rawPathInfo = BS.drop 4 (rawPathInfo req) }
+
+-- | This web server forwards api calls to the API server and otherwise serves
+-- up a static file from the compiled frontend.
+type DebugApi
+  = "api" :> Raw
+  :<|> Raw
+
+debugApi :: Proxy DebugApi
+debugApi = Proxy
+
+main :: IO ()
+main = do
+  apiPort <- read <$> getEnv "B21_API_PORT"
+  frontendPath <- getEnv "B21_FRONTEND_PATH"
+  port <- read <$> getEnv "B21_DEBUG_PORT"
+
+  let dest = ProxyDest "localhost" apiPort
+
+  manager <- newManager defaultManagerSettings
+  run port $ logStdoutDev $ serve debugApi $ server dest frontendPath manager where
+    server :: ProxyDest -> FilePath -> Manager -> Server DebugApi
+    server dest frontendPath manager = apiCall :<|> static where
+      apiCall :: Tagged Handler Application
+      apiCall = Tagged (waiProxyTo (forwardRequest dest) defaultOnExc manager)
+
+      static :: Tagged Handler Application
+      static = serveDirectoryWebApp frontendPath
---
+++ debug/debug.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# builds the frontend
+# builds the backend and runs it in the background
+# builds the debug server and runs it
+
+set -e
+
+export B21_DEBUG_PORT=8080
+export B21_API_PORT=8082
+export B21_FRONTEND_PATH="../frontend/_site"
+
+(cd ../frontend ; ./build.sh)
+(cd ../backend ; exec ./debug.sh) &
+
+echo "starting debug server on port $B21_DEBUG_PORT ..."
+
+exec stack exec b21-debug-exe
