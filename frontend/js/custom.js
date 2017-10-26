2bcbd06da9fd0ef662333db2e354ca99ffbaab6a
Jacob Errington <git@mail.jerrington.me>
2017-10-26 17:31:10 -0400

    comment js code and improve date mangling
    No need to use `.startOf('day')`.

--- frontend/js/custom.js
+++ frontend/js/custom.js
@@ -1,19 +1,26 @@
 'use strict';
 
 $(function() {
+    // fetch the calendar of events from the API
     $.get({
         url: '/api/get_calendar',
         dataType: 'json',
     })
+    // if that succeeds, then we render the calendar
     .done(function(res) {
         $('#calendar').clndr({
             events: _(res).map(function(event) {
-                event.date = moment(event.date).local().startOf('day').format('YYYY-MM-DD');
+                // convert the event time from UTC to localtime, and throw away
+                // the time info to just extract the date.
+                event.date = moment(event.date).local().format('YYYY-MM-DD');
+                return event;
             }),
             template: $('#calendar-template').html(),
         });
     })
+    // if it fails then we log the error and say sorry.
     .fail(function(res) {
         console.log(res);
+        $('#calendar').text('Unable to load the calendar.');
     });
 });
