2dc15b07064e424d420dad043792fb16840b93da
Jacob Errington <git@mail.jerrington.me>
2017-10-26 17:29:43 -0400

    parametrize the debug scripts and don't use localhost
    For some reason connecting to localhost on my computer wouldn't work,
    but 127.0.0.1 works fine. What the fuck?
    
    The debug scripts how have an --offline switch that disables `yarn
    install`, which fails if you have no internet connection.

--- backend/debug.sh
+++ backend/debug.sh
@@ -5,10 +5,19 @@ echo "starting ical server..."
 (cd ../ics ; python3 -m http.server 8084) &
 
 if test -z "$B21_API_POST" ; then
-    export B21_API_PORT=8082
+    B21_API_PORT=8082
 fi
-export B21_EMAIL_FILEPATH=emails.txt
-export B21_CALENDAR_URI="http://localhost:8084/one-event.ics"
+export B21_API_PORT
+
+if test -z "$B21_EMAIL_FILEPATH" ; then
+    B21_EMAIL_FILEPATH=emails.txt
+fi
+export B21_EMAIL_FILEPATH
+
+if test -z "$B21_CALENDAR_URI" ; then
+    B21_CALENDAR_URI="http://127.0.0.1:8084/one-event.ics"
+fi
+export B21_CALENDAR_URI
 
 echo "starting b21 webservice on port $B21_API_PORT"
 
--- debug/debug.sh
+++ debug/debug.sh
@@ -10,8 +10,8 @@ export B21_DEBUG_PORT=8080
 export B21_API_PORT=8082
 export B21_FRONTEND_PATH="../frontend/_site"
 
-(cd ../frontend ; ./build.sh)
-(cd ../backend ; exec ./debug.sh) &
+(cd ../frontend ; ./build.sh "$@")
+(cd ../backend ; exec ./debug.sh "$@") &
 
 echo "starting debug server on port $B21_DEBUG_PORT ..."
 
