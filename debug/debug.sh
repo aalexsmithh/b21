28001d02f5be8d2ee55de4868e12694bc37309fb
Jacob Errington <git@mail.jerrington.me>
2018-02-14 16:11:34 -0500

    refactor debug environment variables

--- debug/debug.sh
+++ debug/debug.sh
@@ -6,10 +6,7 @@
 
 set -e
 
-export B21_DEBUG_PORT=8080
-export B21_API_PORT=8082
-export B21_FRONTEND_PATH="../frontend"
-export B21_CALENDAR_URI="https://calendar.google.com/calendar/ical/730gi5f1v146d8h9mrno43kq7c%40group.calendar.google.com/public/basic.ics"
+source debug.env
 
 mkdir -p ../frontend/static
 
